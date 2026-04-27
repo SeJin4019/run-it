010a89be9578ec1cad4b4c6a0edc6abc6a900135
DESKTOP-VIC8JF6\KSJ <sj.kim4211@hnix.co.kr>
2026-04-27 13:49:16 +0900

    test

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vuetify from 'vite-plugin-vuetify'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/run-it/', // GitHub Pages 배포를 위한 설정 (저장소 이름)
   plugins: [
     vue(),
     vuetify({ autoImport: true }),
